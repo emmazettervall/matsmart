a1c8c04e8eae35ae94111a688ed5632d9924a16f
Emma Zettervall <emma.zettervall@hotmail.com>
2021-11-03 16:43:33 +0100

    påbörjat första sidan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import sok from './searchpic.png';
 
 function App() {
+
+  //håll textrutan uppdaterad
+  const [text1, setText] = useState("");
+
+  function changeInput(event) {
+    setText(event.target.value);
+  }
+
+  //<img src={sok} alt="fel"/>
+  
   return (
     <div className="App">
-      <header className="App-header">
-     hej
+      <header>
+     Butiker 
       </header>
-    </div>
+      <div >
+     
+      <input onChange={changeInput}  placeholder='Sök efter matbutiker...' style={{height:30, width:300, margin:10}}/>
+      <img src={sok} alt="fel"/></div>
+      <h1>Vill du handla mat till bra priser och värna om miljön?</h1>
+      <h2> Ta en titt på matbutikerna nedanför! </h2>
+     
+      </div>
+  
   );
 }
 
